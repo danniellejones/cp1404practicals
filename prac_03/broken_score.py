d93b421bc3c46d52bee5af89c0eccc87c28549f2
Dannielle Jones <dannielle.jones1@my.jcu.edu.au>
2021-08-19 19:28:47 +1000

    Refactor broken score program to use functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,14 +1,25 @@
 """
 CP1404/CP5632 - Practical 1 - Dannielle Jones
-Broken program to determine score status
+Get score and determine score result.
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    score = float(input("Enter score: "))
+    result = determine_result(score)
+    print(result)
+
+
+def determine_result(score):
+    """Determine the result of the score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
