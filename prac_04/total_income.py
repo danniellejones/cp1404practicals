2729bc647c301eea09c2f0c98ac671e8a4914fb1
Dannielle Jones <dannielle.jones1@my.jcu.edu.au>
2021-08-24 13:11:14 +1000

    Add total income program

---
+++ prac_04/total_income.py
@@ -0,0 +1,24 @@
+"""
+CP1404 | Prac_04 | Dannielle Jones
+Calculate the monthly cumulative totals for incomes.
+"""
+
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    months = int(input("How many months? "))
+
+    for month in range(1, months + 1):
+        income = float(input("Enter income for month {} : ".format(str(month))))
+        incomes.append(income)
+
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, months + 1):
+        income = incomes[month - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
+
+main()
