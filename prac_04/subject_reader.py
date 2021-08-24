d728b37a1544b85025509cf7644836a4a9f15c41
Dannielle Jones <dannielle.jones1@my.jcu.edu.au>
2021-08-24 14:12:23 +1000

    Modify get data function to return data and add function to print subject details

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,31 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    print_subject_details(data)
+
+
+def print_subject_details(data):
+    for record in data:
+        print("{} is taught by {} and has {} students".format(record[0], record[1], record[2]))
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        # print(parts)  # See if that worked
+        # print("----------")
+        data.append(parts)
     input_file.close()
+    # print(data)  # Checks if parts has been added to the list
+    return data
 
 
 main()
