dcd6839270017e5abd44cf0e7e3e3cdf7f185597
Dannielle Jones <dannielle.jones1@my.jcu.edu.au>
2021-08-26 20:18:38 +1000

    Refactor quick picks program to use functions

--- prac_04/quick.picks.py
+++ prac_04/quick.picks.py
@@ -4,20 +4,37 @@ Lottery ticket generator.
 """
 import random
 
-QUICK_PICKS = []
+NUMBERS_PER_LINE = 6
+MINIMUM = 1
+MAXIMUM = 45
 
-number_of_quick_picks = int(input("How many quick picks? "))
-for i in range(number_of_quick_picks):
-    quick_pick_numbers = []
-    for j in range(6):
-        number = random.randint(1, 45)
-        while number in quick_pick_numbers:
-            number = random.randint(1, 45)
-        quick_pick_numbers.append(number)
-        quick_pick_numbers.sort()
-    QUICK_PICKS.append(quick_pick_numbers)
 
-for quick_pick in QUICK_PICKS:
-    for number in quick_pick:
-        print("{:2}".format(number), end=" ")
-    print()
+def main():
+    """Generate lottery quick picks from random numbers."""
+    number_of_quick_picks = int(input("How many quick picks? "))
+    while number_of_quick_picks < 0:
+        print("You cannot generate negative quick picks.")
+        number_of_quick_picks = int(input("How many quick picks? "))
+
+    quick_picks = []
+    for i in range(number_of_quick_picks):
+        quick_pick = []
+        for j in range(NUMBERS_PER_LINE):
+            number = random.randint(MINIMUM, MAXIMUM)
+            while number in quick_pick:
+                number = random.randint(MINIMUM, MAXIMUM)
+            quick_pick.append(number)
+            quick_pick.sort()
+        quick_picks.append(quick_pick)
+    print_quick_picks(quick_picks)
+
+
+def print_quick_picks(quick_picks):
+    """Print quick pick numbers."""
+    for quick_pick in quick_picks:
+        for number in quick_pick:
+            print("{:2}".format(number), end=" ")
+        print()
+
+
+main()
