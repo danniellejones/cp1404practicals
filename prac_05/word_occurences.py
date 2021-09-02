6d35f34663c2b31f7fbc314c4168eac7a347fe6e
Dannielle Jones <dannielle.jones1@my.jcu.edu.au>
2021-09-02 15:30:23 +1000

    Modify sort to use keys from dictionary

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -2,20 +2,14 @@
 CP1404 | Prac_05 | Dannielle Jones
 Count the occurrences of words in a string.
 """
-from operator import itemgetter
 
 word_to_count = {}
 text = input("Text: ")
+# text = "this is a collection of words of nice words this is a fun thing it is"
 words = text.split()
 for word in words:
     word_to_count[word] = word_to_count.get(word, 0) + 1
-word_to_count_list = [(word, count) for word, count in word_to_count.items()]
-word_to_count_list.sort(key=itemgetter(0))
-longest_word = max([len(word[0]) for word in word_to_count_list])
-for word, count in word_to_count_list:
-    print("{:{}} : {}".format(word, longest_word, count))
-
-
-
-
-
+words = sorted(word_to_count, key=word_to_count.get(0))
+max_length = max([len(word[0]) for word in words])
+for word in words:
+    print("{:{}} : {}".format(word, max_length, word_to_count[word]))
