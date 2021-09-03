88ca271bbc9fab5bcb277e15decb3d8d7ce8c4d4
Dannielle Jones <dannielle.jones1@my.jcu.edu.au>
2021-09-03 19:20:47 +1000

    Update word occurences sorting

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -4,12 +4,13 @@ Count the occurrences of words in a string.
 """
 
 word_to_count = {}
-text = input("Text: ")
-# text = "this is a collection of words of nice words this is a fun thing it is"
+# text = input("Text: ")
+text = "this is a collection of words of nice words this is a fun thing it is"
 words = text.split()
 for word in words:
     word_to_count[word] = word_to_count.get(word, 0) + 1
-words = sorted(word_to_count, key=word_to_count.get(0))
-max_length = max([len(word[0]) for word in words])
+words = list(word_to_count.keys())
+words.sort()
+max_length = max([len(word) for word in words])
 for word in words:
     print("{:{}} : {}".format(word, max_length, word_to_count[word]))
