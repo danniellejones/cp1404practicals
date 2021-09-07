e7a4c61245964f80e49e97d1c8eb3a803d8ca997
Dannielle Jones <dannielle.jones1@my.jcu.edu.au>
2021-09-07 13:46:48 +1000

    Update Car class to use name and str method

--- prac_06/car.py
+++ prac_06/car.py
@@ -7,13 +7,17 @@ Car class example.
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, fuel=0, name=""):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
         self.fuel = fuel
         self.odometer = 0
+        self.name = name
+
+    def __str__(self):
+        return "Car name={}, fuel={}, odometer={}".format(self.name, self.fuel, self.odometer)
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
