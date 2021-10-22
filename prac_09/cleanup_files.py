039826874e2a001ca2217e1445b2b05988f1d95a
Dannielle Jones <dannielle.jones1@my.jcu.edu.au>
2021-10-22 14:31:02 +1000

    Add cleanup files program

---
+++ prac_09/cleanup_files.py
@@ -0,0 +1,37 @@
+"""
+CP1404 | Prac_09 | Dannielle Jones
+Demos of various os module examples
+"""
+import os
+
+
+def main():
+    """Demo os module functions."""
+    print("Starting directory is: {}".format(os.getcwd()))
+
+    # Change to desired directory
+    os.chdir('Lyrics/Christmas')
+
+    # Print a list of all files in current directory
+    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
+
+    # Loop through each file in the (current) directory
+    for filename in os.listdir('.'):
+        # Ignore directories, just process files
+        if os.path.isdir(filename):
+            continue
+
+        new_name = get_fixed_filename(filename)
+        print("Renaming {} to {}".format(filename, new_name))
+
+        # Rename file to new name - in place
+        # os.rename(filename, new_name)
+
+
+def get_fixed_filename(filename):
+    """Return a 'fixed' version of filename."""
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    return new_name
+
+
+main()
