f9bc9fc65de9521cfddaad920ff6d82fb40c1d47
Dannielle Jones <dannielle.jones1@my.jcu.edu.au>
2021-10-23 17:16:45 +1000

    Modify clean up files program

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -6,31 +6,38 @@ import os
 
 
 def main():
-    """Demo os module functions."""
-    print("Starting directory is: {}".format(os.getcwd()))
-
     # Change to desired directory
-    os.chdir('Lyrics/Christmas')
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        print("Directory:", directory_name)
+        print("\tcontains subdirectories:", subdirectories)
+        print("\tand files:", filenames)
+        print("(Current working directory is: {})".format(os.getcwd()))
 
-    # Print a list of all files in current directory
-    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
-
-    # Loop through each file in the (current) directory
-    for filename in os.listdir('.'):
-        # Ignore directories, just process files
-        if os.path.isdir(filename):
-            continue
-
-        new_name = get_fixed_filename(filename)
-        print("Renaming {} to {}".format(filename, new_name))
-
-        # Rename file to new name - in place
-        # os.rename(filename, new_name)
+        # Loop to rename the files
+        for filename in filenames:
+            new_name = get_fixed_filename(filename)
+            print("Renaming {} to {}".format(filename, new_name))
+            # full_name = os.path.join(directory_name, filename)
+            # new_name = os.path.join(directory_name, new_name)
+            # os.rename(full_name, new_name)
 
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    new_name = filename
+    if "_" not in new_name:
+        new_name = list(new_name)
+        for i, char in enumerate(new_name):
+            if char == '.':
+                break
+            if char.isupper() and i != 0 and new_name[i-1] != " " and new_name[i-1] != "(":
+                new_name[i] = f"_{char}"
+        new_name = "".join(new_name)
+        new_name = new_name.replace(".TXT", ".txt")
+    if " " in new_name or (" " not in new_name and "_" not in new_name):
+        new_name = new_name.title()
+        new_name = new_name.replace(" ", "_").replace(".Txt", ".txt")
     return new_name
 
 
